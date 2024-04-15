d96f2a6faf73c7dcecd7d6ca082c564d6dbf44c9
Thanh Duy <duythanhpham2603@gmail.com>
2024-04-16 00:44:49 +0700

    feature(home): render real products

--- src/services/axios/actions/product.action.ts
+++ src/services/axios/actions/product.action.ts
@@ -4,27 +4,27 @@ import { api } from '..'
 
 // fetch all of products
 export const getProducts = () => {
-    return new Promise<Product[]>((resolve) => {
+    return new Promise<Product[]>((resolve, reject) => {
         server('/dishes', { cache: 'no-store' })
             .then((res) => resolve(res.data as Product[]))
-            .catch((error) => console.error(error))
+            .catch((error) => reject(error))
     })
 }
 
 // fetch menu
 export const getMenu = () => {
-    return new Promise<Menu[]>((resolve) => {
+    return new Promise<Menu[]>((resolve, reject) => {
         server('/menus', { cache: 'no-store' })
             .then((res) => resolve(res.data as Menu[]))
-            .catch((err) => console.error(err))
+            .catch((err) => reject(err))
     })
 }
 
 //fetch product by slugName
 export const getProductBySlugname = (slug: string) => {
-    return new Promise<Product>((resolve) => {
+    return new Promise<Product>((resolve, reject) => {
         api.get(`/dishes/slug/${slug}`)
             .then((res) => res.data as Product)
-            .catch((error) => console.error(error))
+            .catch((error) => reject(error))
     })
 }
