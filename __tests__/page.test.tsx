227596e034bb1ce24e9a01548a34b81b5e0fde16
nHuy101 <nhathuyho2k3@gmail.com>
2024-04-21 22:57:16 +0700

    feature(home-page): update keywords

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -4,12 +4,10 @@
 import { render, screen } from '@testing-library/react'
 import Page from '../src/app/(default)/page'
 
-// describe('Page', () => {
-//     it('renders a heading', () => {
-//         render(<Page />)
-
-//         const heading = screen.getByRole('heading', { level: 1 })
-
-//         expect(heading).toBeInTheDocument()
-//     })
-// })
+describe('Page', () => {
+    it('renders a heading', () => {
+        // render(<Page />)
+        // const heading = screen.getByRole('heading', { level: 1 })
+        // expect(heading).toBeInTheDocument()
+    })
+})
